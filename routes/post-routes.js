34fea88aab5f2669c6faecf47794b5f2c90f8d0d
Galikbarov Eugene <52076316+FableII@users.noreply.github.com>
2022-08-11 18:46:09 +0300

    update mvc

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,88 +1,23 @@
-const express = require('express');
-const Post = require('../models/post');
-const createPath = require('../helper/create-path');
-
+const express = require("express");
 const router = express.Router();
+const {
+  getHome,
+  getPost,
+  deletePost,
+  getEditPost,
+  putEditPost,
+  getPosts,
+  postPost,
+  getAddPost,
+} = require("../controller/post-controller");
 
-router.get('/', (req, res) => {
-    const title = 'Home';
-    res.render(createPath('index'), { title });
-  });
-  
-  router.get('/posts/:id', (req, res) => {
-    const title = 'Post';
-    Post
-      .findById(req.params.id)
-      .then((post) => res.render(createPath('post'), { post, title }))
-      .catch((error) => {
-        console.log(error);
-        res.render(createPath('error'), { title: 'Error' });
-      });
-  });
-  
-  router.delete('/posts/:id', (req, res) => {
-      const title = 'Post';
-      Post
-        .findByIdAndDelete(req.params.id)
-        .then((result) => {
-          res.sendStatus(200);
-        })
-        .catch((error) => {
-          console.log(error);
-          res.render(createPath('error'), { title: 'Error' });
-        });
-    });
-  
-    router.get('/edit/:id', (req, res) => {
-      const title = 'Edit Post';
-      Post
-        .findById(req.params.id)
-        .then((post) => res.render(createPath('edit-post'), { post, title }))
-        .catch((error) => {
-          console.log(error);
-          res.render(createPath('error'), { title: 'Error' });
-        });
-    });
-  
-    router.put('/edit/:id', (req, res) => {
-      const {title, author, text} = req.body;
-      const { id } = req.params;
-      Post
-        .findByIdAndUpdate(id, {title, author, text})
-        .then((result) => res.redirect(`/posts/${id}`))
-        .catch((error) => {
-          console.log(error);
-          res.render(createPath('error'), { title: 'Error' });
-        });
-    });
-  
-  router.get('/posts', (req, res) => {
-    const title = 'Posts';
-    Post
-      .find()
-      .sort({ createdAt: -1 })
-      .then((posts) => res.render(createPath('posts'), { posts, title }))
-      .catch((error) => {
-        console.log(error);
-        res.render(createPath('error'), { title: 'Error' });
-      });
-  });
-  
-  router.post('/add-post', (req, res) => {
-    const { title, author, text } = req.body;
-    const post = new Post({ title, author, text });
-    post
-      .save()
-      .then((result) => res.redirect('/posts'))
-      .catch((error) => {
-        console.log(error);
-        res.render(createPath('error'), { title: 'Error' });
-      })
-  });
-  
-  router.get('/add-post', (req, res) => {
-    const title = 'Add Post';
-    res.render(createPath('add-post'), { title });
-  });
-  
-  module.exports = router;
+router.get("/", getHome);
+router.get("/posts/:id", getPost);
+router.delete("/posts/:id", deletePost);
+router.get("/edit/:id", getEditPost);
+router.put("/edit/:id", putEditPost);
+router.get("/posts", getPosts);
+router.post("/add-post", postPost);
+router.get("/add-post", getAddPost);
+
+module.exports = router;
--- test.js
+++
@@ -1,8 +0,0 @@
-const userName = 'Eugene';
-
-const sayHi = (userName) => `Hello, ${userName}`;
-
-module.exports = {
-    userName,
-    sayHi,
-}
